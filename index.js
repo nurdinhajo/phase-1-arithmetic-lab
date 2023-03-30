833b2c3c48a99696c03c32f9231a89bfe0714dad
nuru <nurudiin222@gmail.com>
2023-03-30 11:15:06 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+
+const num1 = 6.2;
+const num2 = 10;
+const multiply = num1 * num2
+
+
+let random = Math.round(Math.random() *10 + 1)
+
+const num3 = 1000;
+const num4 = 4;
+const mod = num4 % num3;
+
+const max = Math.max(5 , 10 , 15 , 20)
+
